0d69d0136592fbc9f877cbce42586bb68ef9a291
imedardia <benromdhane.imed@gmail.com>
2017-10-10 14:53:38 +0200

    Add Timestamp feature to logging.
    Signed-off-by: imedardia <benromdhane.imed@gmail.com>

--- deps/logger/lib_logger.cpp
+++ deps/logger/lib_logger.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstdio>
 #include <stdarg.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include "logger/lib_logger.h"
 
 using namespace std;
+namespace pt = boost::posix_time;
 
 enum priority {
 	CeLoggerDebug = 0,
@@ -14,11 +16,22 @@ enum priority {
 };
 
 const char * TabPrio[CeMaxPriorities] ={
-	"Dbg",
-	"Inf",
-	"Wrn",
-	"Err"
+	"DBG",
+	"INF",
+	"WRN",
+	"ERR"
 };
+
+void ALOG_GetTimestamp(std::string &timestamp)
+{
+	pt::ptime now = pt::second_clock::local_time();
+	std::stringstream ss;
+	ss << static_cast<int>(now.date().month()) << "/" << now.date().day()
+	   << "/" << now.date().year() << "-"
+	   << now.time_of_day().hours() << ":" << now.time_of_day().minutes()
+	   << ":" << now.time_of_day().seconds();
+	timestamp = ss.str();
+}
 
 template <priority p>
 void ALOGX(std::string tag, std::string func, std::string message, va_list vargs)
@@ -31,10 +44,12 @@ void ALOGX(std::string tag, std::string func, std::string message, va_list vargs
 	}
 	char buffer[1024]={0};
 	char BufferCore[1024]={0};
+	std::string timestamp;
 	va_list vlist;
 	va_copy(vlist, vargs);
+	ALOG_GetTimestamp(timestamp);
 	vsprintf(BufferCore, message.c_str(), vlist);
-    sprintf(buffer,"%s\t%s\t%s\t%s",tag.c_str(), func.c_str(), TabPrio[p], BufferCore);
+    sprintf(buffer,"%s\t%s\t%s\t%s\t%s",timestamp.c_str(), tag.c_str(), func.c_str(), TabPrio[p], BufferCore);
     cout << buffer << endl;
     va_end(vlist);
 }
